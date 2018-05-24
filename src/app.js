350f1acae8d4fdf6b6f5735315a6b23aafe68aee
Sarah Healey <s.deane.healey@gmail.com>
2018-05-24 10:23:52 -0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+
+
+
+const appRoot = document.getElementById('app');
+ReactDOM.render(<p>hello world</p>,appRoot);
